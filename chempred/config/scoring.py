27f21480415a58818e65f01c4e5c185f4b8fbc67
BernalFA <fabernal1230@gmail.com>
2025-08-14 17:07:44 +0200

    restructure scoring

--- chempred/config/scoring.py
+++ chempred/config/scoring.py
@@ -4,32 +4,36 @@ transformers (as implemented in sklearn, imblearn, and scikit-mol, respectively)
 
 @author: Dr. Freddy A. Bernal
 """
+from types import MappingProxyType
+
 from sklearn.metrics import (
     balanced_accuracy_score, f1_score, roc_auc_score, matthews_corrcoef,
     cohen_kappa_score, precision_score, recall_score,
     r2_score, mean_absolute_error, mean_squared_error, root_mean_squared_error
 )
 
+from chempred.config.settings import Scoring
 from chempred.metrics import prc_auc_score, f0_5_score, f2_score, ef_score
 
 
-CLASSIFICATION_SCORERS = dict(
-    balanced_accuracy=balanced_accuracy_score,
-    f1=f1_score,
-    roc_auc=roc_auc_score,
-    prc_auc=prc_auc_score,
-    mcc=matthews_corrcoef,
-    cohen_kappa=cohen_kappa_score,
-    f05=f0_5_score,
-    f2=f2_score,
-    recall=recall_score,
-    precision=precision_score,
-    ef=ef_score,
-)
-
-REGRESSION_SCORERS = dict(
-    r2=r2_score,
-    mae=mean_absolute_error,
-    mse=mean_squared_error,
-    rmse=root_mean_squared_error
+SCORING = Scoring(
+    classification=MappingProxyType({
+        "balanced_accuracy": balanced_accuracy_score,
+        "f1": f1_score,
+        "roc_auc": roc_auc_score,
+        "prc_auc": prc_auc_score,
+        "mcc": matthews_corrcoef,
+        "cohen_kappa": cohen_kappa_score,
+        "f05": f0_5_score,
+        "f2": f2_score,
+        "recall": recall_score,
+        "precision": precision_score,
+        "ef": ef_score,
+    }),
+    regression=({
+        "r2": r2_score,
+        "mae": mean_absolute_error,
+        "mse": mean_squared_error,
+        "rmse": root_mean_squared_error
+    })
 )
--- chempred/config/settings.py
+++ chempred/config/settings.py
@@ -5,7 +5,8 @@ transformers (as implemented in sklearn, imblearn, and scikit-mol, respectively)
 @author: Dr. Freddy A. Bernal
 """
 from dataclasses import dataclass
-from typing import Optional
+from types import MappingProxyType
+from typing import Callable, Optional
 
 
 @dataclass
@@ -20,3 +21,9 @@ class Defaults:
     classifiers: tuple[str]
     regressors: tuple[str]
     samplers: tuple[str]
+
+
+@dataclass(frozen=True)
+class Scoring:
+    regression: MappingProxyType[str, Callable]
+    classification: MappingProxyType[str, Callable]
