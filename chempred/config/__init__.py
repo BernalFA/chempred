8982d1c24a21c4867c98536d14c587ba08cc6152
BernalFA <fabernal1230@gmail.com>
2025-08-14 17:16:51 +0200

    set importable configuration

---
+++ chempred/config/__init__.py
@@ -0,0 +1,5 @@
+from chempred.config.estimators import (
+    CLASSIFIERS, REGRESSORS, SAMPLING_METHODS, MOL_TRANSFORMERS
+)
+from chempred.config.settings import SimpleConfig
+from chempred.config.scoring import SCORING
