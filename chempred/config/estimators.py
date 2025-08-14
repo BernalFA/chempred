8ce13047075ebce143b831611823de24791ddac0
BernalFA <fabernal1230@gmail.com>
2025-08-14 15:54:54 +0200

    restructure default estimators

--- chempred/config/estimators.py
+++ chempred/config/estimators.py
@@ -4,57 +4,68 @@ transformers (as implemented in sklearn, imblearn, and scikit-mol, respectively)
 
 @author: Dr. Freddy A. Bernal
 """
+from dataclasses import dataclass
+
 from chempred.config.functions import get_ml_estimators, all_estimators_in_package
 
 
-selected_classifiers = [
-    "DummyClassifier",
-    "GaussianNB",
-    "GaussianProcessClassifier",
-    "KNeighborsClassifier",
-    "LogisticRegression",
-    "MLPClassifier",
-    "RandomForestClassifier",
-    "RidgeClassifier",
-    "SVC",
-    "XGBClassifier",
-    "LGBMClassifier",
-]
+@dataclass(frozen=True)
+class Defaults:
+    classifiers: tuple[str]
+    regressors: tuple[str]
+    samplers: tuple[str]
+
+
+DEFAULT_ESTIMATORS = Defaults(
+    classifiers=(
+        "DummyClassifier",
+        "GaussianNB",
+        "GaussianProcessClassifier",
+        "KNeighborsClassifier",
+        "LogisticRegression",
+        "MLPClassifier",
+        "RandomForestClassifier",
+        "RidgeClassifier",
+        "SVC",
+        "XGBClassifier",
+        "LGBMClassifier",
+    ),
+    regressors=(
+        "GaussianProcessRegressor",
+        "KNeighborsRegressor",
+        "LinearRegression",
+        "MLPRegressor",
+        "RandomForestRegressor",
+        "Ridge",
+        "SVR",
+        "XGBRegressor",
+        "LGBMRegressor",
+    ),
+    samplers=(
+        "RandomUnderSampler",
+        "SMOTE",
+    )
+)
 
 
 CLASSIFIERS = [
     est
     for est in get_ml_estimators("classification")
-    if est[0] in selected_classifiers
+    if est[0] in DEFAULT_ESTIMATORS.classifiers
 ]
 
-selected_regressors = [
-    "GaussianProcessRegressor",
-    "KNeighborsRegressor",
-    "LinearRegression",
-    "MLPRegressor",
-    "RandomForestRegressor",
-    "Ridge",
-    "SVR",
-    "XGBRegressor",
-    "LGBMRegressor",
-]
 
 REGRESSORS = [
     est
     for est in get_ml_estimators("regression")
-    if est[0] in selected_regressors
+    if est[0] in DEFAULT_ESTIMATORS.regressors
 ]
 
-selected_sampling_methods = [
-    "RandomUnderSampler",
-    "SMOTE",
-]
 
 SAMPLING_METHODS = [
     est
     for est in all_estimators_in_package("imblearn")
-    if est[0] in selected_sampling_methods
+    if est[0] in DEFAULT_ESTIMATORS.samplers
 ]
 
 MOL_TRANSFORMERS = all_estimators_in_package("scikit_mol")
