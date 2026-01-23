abc3dceef51d0e443b35a91f5212992b2f42f684
BernalFA <fabernal1230@gmail.com>
2026-01-23 16:45:09 +0100

    add docstrings to configuration utilities

--- chempred/config/settings.py
+++ chempred/config/settings.py
@@ -11,6 +11,16 @@ from typing import Callable, Optional, Union, Literal
 
 @dataclass
 class SimpleConfig:
+    """Minimum configuration for a single pipeline. It specifies the algorithm,
+    balancing sampler, and the molecular transformer to be used in a single pipeline.
+
+    Attributes:
+        estimator (tuple): name and ML estimator.
+        sampler (tuple, optional): name and imblearn sampler. If None, no class
+                                   balancing will be applied.
+        transformer (tuple, optional): name and scikit-mol transformer. If None,
+                                       raw features will be used.
+    """
     estimator: tuple
     sampler: Optional[tuple] = None
     transformer: Optional[tuple] = None
@@ -18,6 +28,24 @@ class SimpleConfig:
 
 @dataclass
 class ExplorerConfig:
+    """Full configuration for an Explorer instance (ClassificationExplorer or
+    RegressionExplorer). It specifies which algorithms, transformers, class balancing,
+    and scoring along with preprocessing and selection criteria to use during
+    exploration.
+
+    Attributes:
+        ml_algorithms (list | "all"): ML algorithms to include in exploration.
+        balancing_samplers (list | 'all' | None): data samplers for class imbalance
+                                                  treatment to include.
+        mol_transformers (list | 'all' | None): molecular transformers to include in
+                                                exploration.
+        preprocessing ("StandardScaler" | "NovartisScaler" | "NoScaler" | None): data
+                                preprocessing applied before training the model.
+        n_jobs (int): number of cpu units for pipeline processing.
+        scoring (list | None): names of the scoring functions to use during evaluation.
+        select_best_by (str | list): mode of selection of best performing pipeline.
+                The name of a particular metrics used in `scoring` or 'average'.
+    """
     ml_algorithms: Union[list, Literal["all"]]
     balancing_samplers: Optional[Union[list, Literal["all"]]]
     mol_transformers: Optional[Union[list, Literal["all"]]]
@@ -34,6 +62,13 @@ class ExplorerConfig:
 
 @dataclass(frozen=True)
 class Defaults:
+    """Utility to define estimators made available for exploration.
+
+    Attributes:
+        classifiers (tuple[str]): names of ML algorithms used for classification.
+        regressors (tuple[str]): names of ML algorithms used for regression.
+        samplers (tuple[str]): names of imblearn class balancing sampling.
+    """
     classifiers: tuple[str]
     regressors: tuple[str]
     samplers: tuple[str]
@@ -41,5 +76,13 @@ class Defaults:
 
 @dataclass(frozen=True)
 class Scoring:
+    """Utility to define scoring functions made available for performance evaluation.
+
+    Attributes:
+        regression (MappingProxyType[str, Callable]): names and ML estimators for
+                                                      regression.
+        classification (MappingProxyType[str, Callable]): names and ML estimators for
+                                                          classification.
+    """
     regression: MappingProxyType[str, Callable]
     classification: MappingProxyType[str, Callable]
